ae340b2d344985fa08c41b7804e8c1412ac48586
Thomas Harrison <twh2898@vt.edu>
2021-03-29 00:10:22 -0400

    Updated python template

--- scripts/templates/py/main.py
+++ scripts/templates/py/main.py
@@ -1,13 +1,4 @@
 #!/usr/bin/env python3
 
-
-def main(args):
-    print('Hello World')
-
-
 if __name__ == '__main__':
-    import sys
-    try:
-        main(sys.argv)
-    except KeyboardInterrupt:
-        print('Exiting!')
+    print('Hello World')
