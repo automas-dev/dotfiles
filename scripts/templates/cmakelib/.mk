9477e0f8d6ec0c2fc9b247a129943244cd4bf102
Thomas Harrison <twh2898@vt.edu>
2021-05-03 16:28:01 -0400

    Added -DCMAKE_INSTALL_PREFIX=install to cmakelib template

--- scripts/templates/cmakelib/.mk
+++ scripts/templates/cmakelib/.mk
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cmake -S . -B build -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE
+cmake -S . -B build -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE -DCMAKE_INSTALL_PREFIX=install
 
 ln -s build/compile_commands.json
 
