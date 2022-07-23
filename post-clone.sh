5ca8bb93d3e31089d6aa8066c50e635a69496361
Thomas Harrison <twh2898@vt.edu>
2022-07-23 02:06:49 -0400

    Added ssh key generation

--- post-clone.sh
+++ post-clone.sh
@@ -2,6 +2,8 @@
 
 set -e
 
+git submodule update --init --recursive
+
 sudo apt-get update
 sudo apt-get -y upgrade
 
@@ -10,3 +12,9 @@ sudo apt-get -y install ansible
 ansible-playbook install.yaml
 ./install
 
+xclip -sel clipboard < ~/.ssh/id_rsa.pub
+echo "SSH Key copied to clipboard"
+read -p "Press return after adding your SSH key to https://github.com/settinsg/keys"
+
+git remote set-url origin git@github.com:twh2898/dotfiles.git
+git pull
