84acf2941540ff64bde03b32ff8f995c43653e52
Thomas Harrison <thomas.harrison@torc.ai>
2022-07-23 21:24:32 -0400

    Re-structure and update for bashrc and bash_aliases

--- scripts/templates/cmake/.mk
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-cmake -S . -B build -GNinja -DCMAKE_EXPORT_COMPILE_COMMANDS=TRUE
-
-ln -s build/compile_commands.json
-
--- scripts/templates/cmakelib/cmake/ProjectConfig.cmake
+++
@@ -1,9 +0,0 @@
-include(CMakeFindDependencyMacro)
-
-# TODO: capture @var@ config variables
-
-find_dependency(fmtlib REQUIRED)
-
-# TODO: extra steps
-
-include("${CMAKE_CURRENT_LIST_DIR}/FooBarTargets.cmake")
